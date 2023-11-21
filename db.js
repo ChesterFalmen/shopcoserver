91d0eb29a2b1073b8c5a74a96cb3c2a7dc9ddb40
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-11-21 19:56:20 +0200

    fix get products

--- db.js
+++ db.js
@@ -13,8 +13,19 @@ const options = {
 const client = new MongoClient(config.MONGO_CONNECTION_STRING, options);
 
 async function connect() {
-    await client.connect();
-    console.log('Connected to the database');
+    try {
+        await client.connect();
+        console.log('Connected to the database');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
 }
+
+
+// async function connect() {
+//     await client.connect();
+//     console.log('Connected to the database');
+// }
 
 module.exports = { connect, client };
